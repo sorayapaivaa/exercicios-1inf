f8470a2bda29c452150e4cc4f02b3fb4ce519b71
carla soraya <carla.silva419@aluno.ce.gov.br>
2026-08-28 14:55:08 +0000

    finalizado

--- 1-bimestre/logica-de-programacao/projetos/projeto1-sistema-de-controle-de-pedidos/index.js
+++ 1-bimestre/logica-de-programacao/projetos/projeto1-sistema-de-controle-de-pedidos/index.js
@@ -1,9 +1,42 @@
 // CRIE SUA SOLUÇÃO ABAIXO ================
+const cliente = "Mariana Silva"
+const cidade = "Fortaleza"
+const produto = "Notebook Pro"
+const categoria ="Notebook"
+const preco = 3500
+const quantidade = 2
+const descontoPercentual = 10
+const valorPago = 7000
+const subtotal = 7000
+const valorDesconto = 700
+const valorFinal = 6300
+const troco = valorPago - valorFinal 
+ const resumo =`${cliente} fez um pedido de um ${produto} e recebeu R$ ${troco} de troco tendo R$ ${valorFinal} de valor final`
+
+ console.log(`==================================================
+TECHSTORE SISTEMAS
+RESUMO DO PEDIDO
+==================================================
+cliente:${cliente}
+cidade:${cidade}
+produto:${produto}
+categoria:${categoria}
+preco:${preco}
+quantidade:${quantidade}
+descontoPercentual:${descontoPercentual}
+valorPago:${valorPago}
+subtotal:${subtotal}
+valorDesconto:${valorDesconto}
+valorFinal:${valorFinal}
+troco:${troco}`)
+
+console.log(resumo)
 
 
 
 
 
+ 
 
 // === FIM DO CÓDIGO =======================
 // === NÃO FAZER NADA ABAIXO DESSA LINHA ===
